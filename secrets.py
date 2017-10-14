833b530a8f0ce33c893c5aefb600af23375dfe82
SiraniMcNeill <siranimcneill020@gmail.com>
2017-10-14 16:50:14 +1100

    secrets.py

--- secrets.py
+++ secrets.py
@@ -1,4 +1,4 @@
-C_KEY = "MpoM8pyCYfuoJwMvbYmpr16ad"
-C_SECRET = "acABkkmipSBxw3dY1JblEpbFp2mB4fnoBsX1mN1fTmFx6zFVh0"
-A_TOKEN = "918245280092483586-O2RKRyw9FzIqAmSwOnekBi1WeQR64A4"
-A_TOKEN_SECRET = "SADUimBtSsfdtx9huzioIIieSDxvpcpqTUpqkxgsrOxTx"
+consumer_key = "MpoM8pyCYfuoJwMvbYmpr16ad"
+consumer_secret = "acABkkmipSBxw3dY1JblEpbFp2mB4fnoBsX1mN1fTmFx6zFVh0"
+access_token = "918245280092483586-O2RKRyw9FzIqAmSwOnekBi1WeQR64A4"
+access_secret = "SADUimBtSsfdtx9huzioIIieSDxvpcpqTUpqkxgsrOxTx"
